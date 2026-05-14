2b91a978291470352dde6be7134756c3a4720035
Frank Popham <frankpophamathome@gmail.com>
2026-05-14 14:31:25 +0100

    tweaking attach_ducklake so better arguments names etc.

--- R/attach_ducklake.R
+++ R/attach_ducklake.R
@@ -1,16 +1,16 @@
 #' Attach, create or update a ducklake
 #'
 #' @param conduckdb an existing duckdb connection
-#' @param ducklake_path file name (and directory if not in working directory) of ducklake
-#' @param ducklake_alias short alias to use instead of ducklake_path when referring to ducklake database
-#' @param create_if_not_exists create a new ducklake if it does not already exist (default is FALSE).
-#' If FALSE then attaches in READ_ONLY mode (read_only will be separate option in future)
+#' @param ducklake_filename file name (and directory if not in working directory) of ducklake
+#' @param ducklake_alias short alias to use instead of ducklake_filename when referring to ducklake database
+#' @param create_if_not_exists create a new ducklake if it does not already exist (default is FALSE)
+#' @param read_only connect read only to the ducklake. if create_if_not_exists is TRUE this should be FALSE (default is TRUE)
 #'
 #' @param ducklake_type which SQL software to use as catalog for metadata (duckdb or sqlite)
 #' @param ducklake_encrypt should the ducklake itself be encrypted (for duckdb only - default is TRUE)
 #' @param parquet_encrypt should the parquet files storing tables be encrypted (default is TRUE)
-#' @param parquet_data_path directory where the parquet files are / should be stored (default is "data_files")
-#' @param override_data_path if data path is different to default should this override the default (default is FALSE)
+#' @param parquet_directory directory where the parquet files are / should be stored (default is "data_files")
+#' @param override_parquet_directory if data path is different to default should this overrides the default (default is FALSE)
 #'
 #' @returns Attaches the ducklake to your duckdb connection and with message confirming the ducklake is the default database.
 #' Returns dataframe of attached databases.
@@ -19,11 +19,11 @@
 #' @examples my_con <- connect_duckdb()
 #'
 #' attach_ducklake(my_con,
-#'    ducklake_path = tempfile(pattern = "metadata.ducklake"),
+#'    ducklake_filename = tempfile(pattern = "metadata.ducklake"),
 #'    ducklake_alias = "my_ducklake",
 #'    create_if_not_exists = TRUE,
 #'    ducklake_encrypt = FALSE,
-#'    parquet_data_path = tempdir())
+#'    parquet_directory = tempdir())
 #'
 #' switch_default_database(my_con, "memory")
 #'
@@ -32,18 +32,19 @@
 #' detach_ducklake(my_con, "my_ducklake", "memory")
 
 attach_ducklake <- function(conduckdb,
-                            ducklake_path = NULL,
+                            ducklake_filename  = NULL,
                             ducklake_alias = NULL,
                             create_if_not_exists = FALSE,
+                            read_only = TRUE,
                             ducklake_type = "duckdb",
-                            ducklake_encrypt = TRUE,
-                            parquet_encrypt = TRUE,
-                            parquet_data_path = "data_files",
-                            override_data_path = FALSE) {
+                            ducklake_encrypt = FALSE,
+                            parquet_encrypt = FALSE,
+                            parquet_directory = "data_files",
+                            override_parquet_directory = FALSE) {
 
   check_duckdb_connection(conduckdb)
 
-  stopifnot("ducklake_path should be a character path to a ducklake" = is.character(ducklake_path))
+  stopifnot("ducklake_filename should be a character path to a ducklake" = is.character(ducklake_filename))
 
   stopifnot("ducklake_alias should be a character (such as my_ducklake)" = is.character(ducklake_alias))
 
@@ -57,39 +58,36 @@ attach_ducklake <- function(conduckdb,
 
   cine <- glue::glue_sql("CREATE_IF_NOT_EXISTS {create_if_not_exists}", .con = conduckdb)
 
-  # duckdb
-
-  if (ducklake_type == "duckdb") {
-    dp <- DBI::dbQuoteLiteral(conduckdb, paste0("ducklake:", ducklake_path))
-  }
-
   da <- DBI::SQL(ducklake_alias)
+
+  # type
+
+  dt <- glue::glue_sql("META_TYPE {ducklake_type}", .con = conduckdb)
+
+  df <- DBI::dbQuoteLiteral(conduckdb, paste0("ducklake:", ducklake_filename))
 
   if (ducklake_type == "duckdb" && ducklake_encrypt) {
     de <- DBI::SQL("META_ENCRYPTION_KEY ?")
   }
 
-  # sqlite
-
-  if (ducklake_type == "sqlite") {
-    dp <- DBI::dbQuoteLiteral(conduckdb, paste0("ducklake:sqlite:", ducklake_path))
-  }
 
   pe <- glue::glue_sql("ENCRYPTED {parquet_encrypt}", .con = conduckdb)
 
-  pdp <- glue::glue_sql("DATA_PATH {parquet_data_path}", .con = conduckdb)
+  pd <- glue::glue_sql("DATA_PATH {parquet_directory}", .con = conduckdb)
 
-  odp <- glue::glue_sql("OVERRIDE_DATA_PATH {override_data_path}", .con = conduckdb)
+  odp <- glue::glue_sql("OVERRIDE_DATA_PATH {override_parquet_directory}", .con = conduckdb)
+
+  ro <- glue::glue_sql("READ_ONLY {read_only}", .con = conduckdb)
 
   # build for attach
   if(!create_if_not_exists & ducklake_encrypt) {
-    attach_sql <- glue::glue_sql("ATTACH {dp} AS {da} ({cine}, {de}, READ_ONLY);", .con = conduckdb)
+    attach_sql <- glue::glue_sql("ATTACH {df} AS {da} ({dt}, {cine}, {de}, {ro});", .con = conduckdb)
   } else if (!create_if_not_exists & !ducklake_encrypt) {
-    attach_sql <- glue::glue_sql("ATTACH {dp} AS {da} ({cine}, READ_ONLY);", .con = conduckdb)
+    attach_sql <- glue::glue_sql("ATTACH {df} AS {da} ({dt}, {cine}, {ro});", .con = conduckdb)
   } else if (create_if_not_exists &  ducklake_encrypt) {
-    attach_sql <- glue::glue_sql("ATTACH {dp} AS {da} ({cine}, {pe}, {pdp}, {odp}, {de});", .con = conduckdb)
+    attach_sql <- glue::glue_sql("ATTACH {df} AS {da} ({dt}, {cine}, {pe}, {pd}, {odp}, {de}, {ro});", .con = conduckdb)
   } else {
-    attach_sql <- glue::glue_sql("ATTACH {dp} AS {da} ({cine}, {pe}, {pdp}, {odp});", .con = conduckdb)
+    attach_sql <- glue::glue_sql("ATTACH {df} AS {da} ({dt}, {cine}, {pe}, {pd}, {odp}, {ro});", .con = conduckdb)
   }
 
   # attach
